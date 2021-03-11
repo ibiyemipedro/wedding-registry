a50c30c1f5fc0f1323b842d751703b35d6d076f5
yemipedro07 <yemipedro07@gmail.com>
2021-03-11 22:38:11 +0100

    added payment modal

--- src/app/Landing.js
+++ src/app/Landing.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Modal, Button, Form, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-class Landing extends Component {
-  state = {
-    users: [],
-  };
+const Landing = () => {
+  const [show, setShow] = useState(false);
 
-  render() {
-    return (
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
       <div className="col-12">
         <div className="row">
           {/* top banner */}
@@ -22,7 +24,7 @@ class Landing extends Component {
               </div>
             </div>
           </div>
-
+          {/* body */}
           <div className="col-12 py-5">
             <div className="container">
               <div className="card my-2">
@@ -38,6 +40,8 @@ class Landing extends Component {
                       <button
                         type="button"
                         className="btn btn-dark btn-lg btn-block"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
                       >
                         Add
                       </button>
@@ -55,22 +59,75 @@ class Landing extends Component {
                       <p>NGN 15,000.00</p>
                     </div>
                     <div className="col-3">
-                      <button
-                        type="button"
-                        className="btn btn-dark btn-lg btn-block"
-                      >
+                      <Button variant="dark" onClick={handleShow} block>
                         Add
-                      </button>
+                      </Button>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+
+          {/* modal */}
+          <Modal show={show} onHide={handleClose} centered>
+            <Modal.Header closeButton></Modal.Header>
+            <Modal.Body>
+              <Form className="px-5 py-5">
+                <Form.Group>
+                  <h3>
+                    <strong>Sandwich Maker</strong>
+                  </h3>
+                  <p>NGN 5,000.00 - unit price</p>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formHorizontalEmail">
+                  <Col sm={3}>
+                    <Button variant="dark" block>
+                      -
+                    </Button>
+                  </Col>
+                  <Col sm={6}>
+                    <Form.Control
+                      type="email"
+                      placeholder="Email"
+                      readOnly
+                      value="1"
+                    />
+                  </Col>
+                  <Col sm={3}>
+                    <Button variant="dark" block>
+                      +
+                    </Button>
+                  </Col>
+                </Form.Group>
+                <Form.Group>
+                  <p className="text-right">
+                    <strong>NGN 5,000.00</strong>
+                  </p>
+                </Form.Group>
+                <Form.Group className="pt-5">
+                  <p>
+                    We really appreciate your support, let's get to know you
+                  </p>
+                </Form.Group>
+                <Form.Group as={Row}>
+                  <Col sm={6}>
+                    <Form.Control type="text" placeholder="Name" />
+                  </Col>
+                  <Col sm={6}>
+                    <Form.Control type="text" placeholder="Mobile Number" />
+                  </Col>
+                </Form.Group>
+                <Button variant="dark" block className="mt-4">
+                  Pay Now
+                </Button>
+              </Form>
+            </Modal.Body>
+          </Modal>
         </div>
       </div>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default Landing;
